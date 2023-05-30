9f296c127259a9c6445701d649fbfbf34217c6b0
Cristian Home <chome@codebranch.co>
2023-05-30 01:11:30 +0000

    Setup frontend: - Vue 3 + Vue Router + Pinia. - Testing with Vitest. - Lint and format with ESLint and Prettier.

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+    <HelloWorld msg="You did it!" />
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <RouterView />
+</template>
