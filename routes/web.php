4576b6d24d3b9d1168623cbb045c5b971b583af9
Cristian Home <chome@codebranch.co>
2023-05-30 12:22:11 +0000

    Install Laravel Fortify: Authentication backend.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+Route::get('/#password-reset', function () {
+    return view('app');
+})->name('password.reset');
